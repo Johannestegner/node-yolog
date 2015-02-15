91e2b4e8ec9b6a51c2695b94d44e4dcc1a448c5c
Johannes Tegnér <jitedev@gmail.com>
2015-02-15 20:17:31 +0100

    The Yolog object is no longer a self-executing function, but a regular constructor. Although, the api is intact and a 'new Yolog()' object is exported. Deprecated 'depth' property and introduced a function for it - Readme rewrite pending. Deprecated setcolor function, introduced a new setColor function - Spelling reasons. The get function will no longer return the full tags- object, only works with a tag added, and if it does not exist, undefined is returned. Renamed all private variables/functions to more js-standard name conventions.

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
 /**
  * @description A logging tool for node js.
  * @file main.js
- * @author Johannes Tegnér
- * @contributors Henrik Myntti
  */
-module.exports = (function(){
 
-  var colors = require('./colors.js');
-  var util = require('util');
-  var os = require('os');
+/**
+ * @constructor
+ */
+function Yolog(){
 
-  var shouldPrintFunctionName = false;
-
-  var tags = {
-      trace:    true
-    , debug:    true
-    , error:    true
-    , warning:  true
-    , info:     true
-    , todo:     true
-  };
-
-  var tagcolors = {
-      trace:    colors.textnormal.cyan
-    , debug:    colors.textnormal.blue
-    , error:    colors.textnormal.red
-    , warning:  colors.textnormal.yellow
-    , info:     colors.textnormal.white
-    , todo:     colors.textnormal.green
+  var _printFuncName = false;
+  var _colors   = require('./colors.js');
+  var _util     = require('util');
+  var _eol      = process.platform === 'win32' ? '\r\n' : '\n';
+  var _tags     = {
+    trace: {
+      color: _colors.normal.cyan
+      , active: true
+    }
+    , debug: {
+      color: _colors.normal.blue
+      , active: true
+    }
+    , error: {
+      color: _colors.normal.red
+      , active: true
+    }
+    , warning: {
+      color: _colors.normal.yellow
+      , active: true
+    }
+    , info: {
+      color: _colors.normal.white
+      , active: true
+    }
+    , todo: {
+      color: _colors.normal.green
+      , active: true
+    }
   };
 
  /**
    * Print a debug string formatted as "(date) tag: string with %s|d|j values" where % values will be replaced with passed arguments.
    * @param {string} tag Tag to use.
    * @param {string} string String to print.
-   * @param {string} color Color code.
    * @param {null|undefined|Array} args Arguments.
-   * @param {boolean} toStdErr If the data should be printed to stderr or stdout.
    */
-  var print = function(tag, string, color, args, toStdErr) {
+  var _print = function(tag, string, args) {
+   tag = tag.toLowerCase();
     if(args !== undefined && args !== null) {
       for(var i=0; i<args.length; i++){
-        string = util.format(string, args[i]);
+        string = _util.format(string, args[i]);
       }
     }
     // [TagColor]Tag[/TagColor][Cyan]\tDate[/Cyan]Text\n
-    var out = util.format("%s%s%s%s\t(%s)%s: %s%s", color, tag, colors.reset, colors.textnormal.cyan, (new Date()).toLocaleTimeString(), getCaller(3),  colors.reset, string);
-    if(toStdErr){
+   var tagName = tag.charAt(0).toUpperCase() + tag.slice(1);
+    var out = _util.format("%s%s%s%s\t(%s)%s: %s%s", _tags[tag].color, tagName, _colors.reset, _colors.normal.cyan, (new Date()).toLocaleTimeString(), _getCaller(3),  _colors.reset, string);
+    if(tag === 'error'){
       console.error(out);
     } else {
       console.log(out);
@@ -55,14 +64,13 @@ module.exports = (function(){
 
   /**
    * Get calling function
-   * @param {int} depth Depth to go in callstack.
+   * @param {int} depth Max depth to go in call-stack.
    * @returns {string} Function name.
    */
-  var getCaller = function(depth) {
-    if(!shouldPrintFunctionName) {
+  var _getCaller = function(depth) {
+    if(!_printFuncName) {
       return "";
     }
-
     try {
       var caller = arguments.callee.caller;
       depth = depth === undefined ? 0 : depth;
@@ -77,140 +85,174 @@ module.exports = (function(){
       }
       return "[" + funcName + "]";
     }  catch(e) {
-      return "[Unknown/Strict]";
+      return "[unknown/strict]";
     }
   };
 
-  return {
-    /**
-     * Depth to use when iterating objects.
-     */
-    depth: 3,
-    /**
-     * If function names should be printed in the output, this should be set to true.
-     * Default is true.
-     * @param {boolean} state State to set it to.
-     */
-    showFunctionName: function(state) {
-      shouldPrintFunctionName = state;
-    },
-    /**
-     * Set color of a tag.
-     * @param {string} tag Tag name (debug, trace, error, info or warning).
-     * @param {string} color Color as string (black, red, green, yellow, blue, purple, cyan, white).
-     */
-    setcolor: function(tag, color) {
-      color = color.toLowerCase();
-      tag = tag.toLowerCase();
-      if(colors.textnormal[color] === undefined) {
-        print("error", "Failed to set color of tag with name %s. The color (%s) is not a valid color.", colors.textnormal.red, [tag, color]);
-      } else if(tagcolors[tag] === undefined) {
-        print("error", "Failed to set color of tag with name %s. The tag does not exist.", colors.textnormal.red, [tag]);
-      }
-      tagcolors[tag] = colors.textnormal[color];
-    },
-    /**
-     * Set one or many tags to active or inactive.
-     * @param {boolean} value True if active, false if inactive.
-     * @param {...} args Argument list, one or many string of which to set to given value.
-     */
-    set: function(value, args) {
-      for(var i=1;i<arguments.length;i++) {
-        var tag = arguments[i].toLowerCase();
-        if(tag in tags) {
-          tags[tag] = value;
-        } else {
-          print("error", "Failed to set tag " + tag + " to " + (value ? "Active" : "Inactive") + ", tag do not exist.", colors.textnormal.red, null);
-        }
-      }
-    },
-    /**
-     * Get current state of given tag, or the whole tags object.
-     * @param {undefined|string} tag Tag as string or omitted for whole tags object.
-     * @return {object|boolean} If tag is active, true, else false. Or whole tags object if tag is omitted.
-     */
-    get: function(tag) {
-      if(tag === undefined){
-        return tags;
+  /**
+   * @deprecated 0.9.
+   * @see setObjectMaxDepth
+   * @type {number}
+   */
+  this.depth = 3;
+
+  /**
+   * Set depth to use when printing objects (in trace).
+   * @param {number} value Maximum depth to use.
+   */
+  this.setObjectMaxDepth = function setObjectMaxDepth(value) {
+    // todo: remove this.depth and make it a private variable.
+    this.depth = value;
+  };
+
+  /**
+   * If function names should be printed in the output, this should be set to true.
+   * Default is true.
+   * @param {boolean} state State to set it to.
+   */
+  this.showFunctionName = function showFunctionName(state) {
+    _printFuncName = state;
+  };
+
+  /**
+   * Set color of a tag.
+   * @param {string} tag Tag name (debug, trace, error, info or warning).
+   * @param {string} color Color as string (black, red, green, yellow, blue, purple, cyan, white).
+   * @deprecated as of version 0.9 - Use the `setColor`.
+   * @see setColor
+   */
+  this.setcolor = function setcolor(tag, color) {
+    this.setColor(tag, color);
+  };
+
+  /**
+   * Set color of a tag.
+   * @param {string} tag Tag name (debug, trace, error, info or warning).
+   * @param {string} color Color as string (black, red, green, yellow, blue, purple, cyan, white).
+   */
+  this.setColor = function setColor(tag, color) {
+    color = color.toLowerCase();
+    tag = tag.toLowerCase();
+    if(_colors.normal[color] === undefined) {
+      _print("error", "Failed to set color of tag with name %s. The color (%s) is not a valid color.", [tag, color]);
+    } else if(_tags[tag] === undefined) {
+      _print("error", "Failed to set color of tag with name %s. The tag does not exist.", [tag]);
+    }
+    _tags[tag].color = _colors.normal[color];
+  };
+
+  /**
+   * Set one or many tags to active or inactive.
+   * @param {boolean} value True if active, false if inactive.
+   * @param {...} args Argument list, one or many string of which to set to given value.
+   */
+  this['set'] = function set(value, args) {
+    for(var i=1;i<arguments.length;i++) {
+      var tag = arguments[i].toLowerCase();
+      if(tag in _tags) {
+        _tags[tag].active = value;
       } else {
-        tag = tag.toLowerCase();
-        if(tag in tags) {
-          return tags[tag];
-        }
-      }
-      print("error", "Failed to get tag " + tag + ", tag do not exist.", colors.textnormal.red, null);
-      return false;
-    },
-    /**
-     * Output arguments to trace log.
-     * @param {...} args Argument list.
-     */
-    trace: function(args) {
-      if(tags.trace) {
-        var reg = new RegExp('\r?\n','g');
-        var out = tagcolors.trace + "trace\t(" + (new Date()).toLocaleTimeString() + ")" + getCaller(2) + ": " + colors.reset + tagcolors.trace + " [ length: " + arguments.length + " ] " + colors.reset + os.EOL;
-        for (var i = 0, il = arguments.length; i < il; i++) {
-          out += tagcolors.trace + "["+ i + "]\t" + colors.reset;
-          var text = util.inspect(arguments[i], { showHidden: true, depth: this.depth });
-          out += text.replace(reg, tagcolors.trace + os.EOL + " *\t" + colors.reset) + colors.reset;
-          out += os.EOL;
-        }
-        out += tagcolors.trace + "end trace" + colors.reset;
-        console.log(out);
-      }
-    },
-    /**
-     * Prints a debug string to stdout.
-     * The string uses % placeholders and replaces them with arguments passed.
-     * Possible placeholders: %s - string, %d - number, %j json.
-     * @param {string} str String to print.
-     * @param {...} args Argument list.
-     */
-    debug: function(str, args) {
-      if(tags.debug) {
-        print("Debug", str, tagcolors.debug, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []));
-      }
-    },
-    /**
-     * Prints a error string to stdout.
-     * The string uses % placeholders and replaces them with arguments passed.
-     * Possible placeholders: %s - string, %d - number, %j json.
-     * @param {string} str String to print.
-     * @param {...} args Argument list.
-     */
-    error: function(str, args) {
-      if(tags.error) {
-         print("Error", str, tagcolors.error, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []), true);
-      }
-    },
-    /**
-     * Prints a warning string to stdout.
-     * The string uses % placeholders and replaces them with arguments passed.
-     * Possible placeholders: %s - string, %d - number, %j json.
-     * @param {string} str String to print.
-     * @param {...} args Argument list.
-     */
-    warning: function(str, args) {
-      if(tags.warning) {
-        print("Warning", str, tagcolors.warning, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []));
-      }
-    },
-    /**
-     * Prints a info string to stdout.
-     * The string uses % placeholders and replaces them with arguments passed.
-     * Possible placeholders: %s - string, %d - number, %j json.
-     * @param {string} str String to print.
-     * @param {...} args Argument list.
-     */
-    info: function(str, args) {
-      if(tags.info) {
-        print("Info", str, tagcolors.info, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []));
-      }
-    },
-    todo: function(str, args) {
-      if(tags.todo){
-        print("Todo", str, tagcolors.todo, (args !== undefined ? Array.prototype.slice.call(arguments,1) : []));
+        _print("error", "Failed to set tag " + tag + " to " + (value ? "Active" : "Inactive") + ", tag do not exist.", null);
       }
     }
   };
-})();
+
+  /**
+   * Get current state of given tag.
+   * @param {string} tag Tag as string.
+   * @return {undefined|boolean} If tag is active, true, else false. If tag does not exist, undefined.
+   */
+  this['get'] = function get(tag) {
+    tag = tag.toLowerCase();
+    if(tag in _tags) {
+      return _tags[tag].active;
+    }
+    return undefined;
+  };
+
+  /**
+   * Output arguments to trace log.
+   * @param {...} args Argument list.
+   */
+  this.trace = function trace(args) {
+    if(_tags.trace.active) {
+      var reg = new RegExp('\r?\n','g');
+      var out = _tags.trace.color + "Trace\t(" + (new Date()).toLocaleTimeString() + ")" + _getCaller(2) + ": " + _colors.reset + _tags.trace.color + " [ length: " + arguments.length + " ] " + _colors.reset + _eol;
+      for (var i = 0, il = arguments.length; i < il; i++) {
+        out += _tags.trace.color + "["+ i + "]\t" + _colors.reset;
+        var text = _util.inspect(arguments[i], { showHidden: true, depth: this.depth });
+        out += text.replace(reg, _tags.trace.color + _eol + " *\t" + _colors.reset) + _colors.reset;
+        out += _eol;
+      }
+      out += _tags.trace.color + "end trace" + _colors.reset;
+      console.log(out);
+    }
+  };
+
+  /**
+   * Prints a debug string to stdout.
+   * The string uses % placeholders and replaces them with arguments passed.
+   * Possible placeholders: %s - string, %d - number, %j json.
+   * @param {string} str String to print.
+   * @param {...} args Argument list.
+   */
+  this.debug = function debug(str, args) {
+    if(_tags.debug.active) {
+      _print("debug", str, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []));
+    }
+  };
+
+  /**
+   * Prints a error string to stdout.
+   * The string uses % placeholders and replaces them with arguments passed.
+   * Possible placeholders: %s - string, %d - number, %j json.
+   * @param {string} str String to print.
+   * @param {...} args Argument list.
+   */
+  this.error = function error(str, args) {
+    if(_tags.error.active) {
+       _print("error", str, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []));
+    }
+  };
+
+  /**
+   * Prints a warning string to stdout.
+   * The string uses % placeholders and replaces them with arguments passed.
+   * Possible placeholders: %s - string, %d - number, %j json.
+   * @param {string} str String to print.
+   * @param {...} args Argument list.
+   */
+  this.warning = function warning(str, args) {
+    if(_tags.warning.active) {
+      _print("warning", str, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []));
+    }
+  };
+
+  /**
+   * Prints a info string to stdout.
+   * The string uses % placeholders and replaces them with arguments passed.
+   * Possible placeholders: %s - string, %d - number, %j json.
+   * @param {string} str String to print.
+   * @param {...} args Argument list.
+   */
+  this.info = function info(str, args) {
+    if(_tags.info.active) {
+      _print("info", str, (args !== undefined ? Array.prototype.slice.call(arguments, 1) : []));
+    }
+  };
+
+  /**
+   * Prints a to-do string to stdout.
+   * The string uses % placeholders and replaces them with arguments passed.
+   * Possible placeholders: %s - string, %d - number, %j json.
+   * @param {string} str String to print.
+   * @param {...} args Argument list.
+   */
+  this.todo = function todo(str, args) {
+    if(_tags.todo.active){
+      _print("todo", str, (args !== undefined ? Array.prototype.slice.call(arguments,1) : []));
+    }
+  };
+}
+
+module.exports = new Yolog();
